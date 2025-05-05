d07dfa10aa787dd727438ba3f13e5b3025afcd70
Roxtop07 <148778185+Roxtop07@users.noreply.github.com>
2025-05-05 22:48:49 +0530

    Update openai.ts

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,5 +1,5 @@
 import { OpenAI } from "openai";
 
 export const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
